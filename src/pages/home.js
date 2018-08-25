bd30c1f6d557f8540575ab1903604472a85398dd
John Grishin <r@exah.ru>
2018-08-26 01:26:48 +0300

    ♻️ Update picture route

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,6 +7,11 @@ const Home = ({ data, isLoading }) => (
   </div>
 )
 
-export default withData((props) => new Promise((resolve) =>
-  setTimeout(() => resolve({ data: 'ok' }), 200)
-))(Home)
+export default withData(
+  (props) => new Promise((resolve) =>
+    setTimeout(() => resolve({ data: 'ok' }), 200)
+  ),
+  (prev, next) =>
+    prev.match.params.lang !== next.match.params.lang ||
+    prev.match.params.pictureName !== next.match.params.pictureName
+)(Home)
