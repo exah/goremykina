838a784e39ef86a697ff3baddedb09b9bfebf75e
John Grishin <r@exah.ru>
2018-08-28 01:38:44 +0300

    🤡 Add photo to `About` page mock

--- src/data/about.js
+++ src/data/about.js
@@ -1,11 +1,21 @@
 import { dedent } from '../utils'
 
+const photo = {
+  url: '/photo.jpg',
+  width: 2912,
+  height: 4368,
+  ratio: 0.666666667
+}
+
 const content = dedent`
+  Ирина Горемыкина
+
   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu libero libero, sit amet commodo sem. Proin a quam vulputate enim consequat sollicitudin. Etiam vitae nunc augue. Sed ac arcu nibh. Suspendisse a lobortis nulla. Maecenas nunc erat, consectetur quis sagittis et, sodales ac nisl. Proin vitae nibh sed massa blandit commodo. Sed ornare tincidunt nibh, vel malesuada orci pellentesque sit amet. Pellentesque velit urna, porttitor a eleifend sit amet, feugiat ac velit. Nulla ut nunc sit amet lorem dictum suscipit. Maecenas ullamcorper feugiat erat nec ornare.
 
   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu libero libero, sit amet commodo sem. Proin a quam vulputate enim consequat sollicitudin. Etiam vitae nunc augue. Sed ac arcu nibh. Suspendisse a lobortis nulla. Maecenas nunc erat, consectetur quis sagittis et, sodales ac nisl. Proin vitae nibh sed massa blandit commodo. Sed ornare tincidunt nibh, vel malesuada orci pellentesque sit amet. Pellentesque velit urna, porttitor a eleifend sit amet, feugiat ac velit. Nulla ut nunc sit amet lorem dictum suscipit. Maecenas ullamcorper feugiat erat nec ornare.
 `
 
 export {
+  photo,
   content
 }
