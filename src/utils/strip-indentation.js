8a0ed0226fb43d1e5c780b0ec58a1585a5104eec
John Grishin <r@exah.ru>
2018-08-30 00:56:37 +0300

    ♻️ Improve `strip-indentation` fns

--- src/utils/strip-indentation.js
+++ src/utils/strip-indentation.js
@@ -1,29 +1,32 @@
-const getIndentation = (src) => {
-  const lengths = src.split('\n')
+const getIndent = (lines) => {
+  const lengths = lines
     .filter(line => line.trim().length !== 0)
     .map((str) => str.length - str.trimLeft().length)
 
-  return Math.min(...lengths)
+  const result = Math.min(...lengths)
+
+  return result === Infinity ? 0 : result
 }
 
 const toArray = (src) => Array.isArray(src) ? src : src ? [ src ] : []
 
 const dedent = (strings, ...values) => {
-  const indentation = getIndentation(strings.join(''))
+  const stripIndent = getIndent(strings.join('').split('\n'))
 
   const parts = []
 
   for (let stringIndex = 0; stringIndex < strings.length; stringIndex++) {
     const trimmed = strings[stringIndex]
       .split('\n')
-      .map((str, strIndex) => stringIndex === 0 || strIndex > 0 ? str.substr(indentation) : str)
-      .join('\n')
+      .map((line, lineIndex) => stringIndex === 0 || lineIndex > 0 ? line.substring(stripIndent) : line)
 
-    parts.push(trimmed)
+    parts.push(trimmed.join('\n'))
 
     if (stringIndex < values.length) {
+      const indent = getIndent(trimmed.slice(1))
+
       parts.push(
-        toArray(values[stringIndex]).join('')
+        toArray(values[stringIndex]).join('\n' + ''.padStart(indent))
       )
     }
   }
