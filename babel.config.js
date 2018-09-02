42100163eb0878922a053ec1e857230663b36223
John Grishin <r@exah.ru>
2018-09-02 23:37:32 +0300

    🔧 Update `babel.config.js`

---
+++ babel.config.js
@@ -0,0 +1,29 @@
+const config = require('config')
+
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env', {
+        useBuiltIns: 'usage',
+        modules: false
+      }
+    ],
+    '@babel/preset-react'
+  ],
+  plugins: [
+    'babel-plugin-dynamic-import-node',
+    '@babel/plugin-proposal-class-properties',
+    [
+      '@babel/plugin-transform-runtime', {
+        useESModules: true
+      }
+    ],
+    [
+      'emotion', {
+        sourceMap: config.isDev,
+        autoLabel: config.isDev,
+        hoist: config.isProd
+      }
+    ]
+  ]
+}
