24173b81c03853e268ec3ec7d9893b2e1ee70660
John Grishin <r@exah.ru>
2018-12-25 02:50:22 +0300

    ♻️ Simplify dedent code

--- src/utils.js
+++ src/utils.js
@@ -18,48 +18,25 @@ export const createApi = (baseUrl = '') => {
   return api
 }
 
-const renderMarkdown = (src = '') =>
+export const renderMarkdown = (src = '') =>
   src.split('\n\n').map((children, index) => (
     <Text key={index} as='p' mgb={2}>
       {children}
     </Text>
   ))
 
-const getIndent = (lines) => {
-  const lengths = lines
-    .filter(line => line.trim().length !== 0)
-    .map((str) => str.length - str.trimLeft().length)
-
-  const result = Math.min(...lengths)
-
-  return result === Infinity ? 0 : result
+export const getIndentation = (str) => {
+  const match = str.match(/^[ \t]*(?=\S)/gm)
+  return match ? Math.min(...match.map(x => x.length)) : 0
 }
 
-const dedent = (strings, ...values) => {
-  const stripIndent = getIndent(strings.join('').split('\n'))
+export const dedent = (input, ...values) => {
+  const indentation = getIndentation(input.join(''))
+  const joined = input.map((str, i) => str + (toArr(values[i]).join(''))).join('')
 
-  const parts = []
-
-  for (let stringIndex = 0; stringIndex < strings.length; stringIndex++) {
-    const trimmed = strings[stringIndex]
-      .split('\n')
-      .map((line, lineIndex) => stringIndex === 0 || lineIndex > 0 ? line.substring(stripIndent) : line)
-
-    parts.push(trimmed.join('\n'))
-
-    if (stringIndex < values.length) {
-      const indent = getIndent(trimmed.slice(1))
-
-      parts.push(
-        toArr(values[stringIndex]).join('\n' + ''.padStart(indent))
-      )
-    }
+  if (indentation === 0) {
+    return joined
   }
 
-  return parts.join('').trim()
-}
-
-export {
-  renderMarkdown,
-  dedent
+  return joined.replace(new RegExp(`^[ \\t]{${indentation}}`, 'gm'), '').trim() + '\n'
 }
