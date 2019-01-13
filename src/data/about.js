ce358a5eca71907037c310b06d95f790fe95515d
John Grishin <r@exah.ru>
2019-01-14 00:26:18 +0300

    💬 Update texts in about page

--- src/data/about.js
+++ src/data/about.js
@@ -21,7 +21,7 @@ const content = {
 
     Немного уединения - это попытка очистить свое сознание от социальных рамок, посмотреть на наш мир другими глазами, увидеть его первозданность, почувствовать ответственность и глубину происходящих событий нашей жизни. Возвращаясь в Москву я переоцениваю многие ситуации и привожу огромный запас вдохновения и идей.
 
-    Почти все мои работы посвящены Крыму, местам в которых жили и писали свои произведения художники, поэты и писатели прошлого века, такие как художник И. Айвазовский, К. Богоевский, поэт - художник М. Волошин, писатель А. Грин. В Крыму делаю много зарисовок, эскизов, пишу длительные этюды. Впоследствии этот творческий материал использую в своих работах. В Москве пишу большие работы, очень часто сериями, например серии работ «Сны и видения», «Иллюзии», «Музыка моря», «Лунная соната».
+    Многие мои работы посвящены Крыму, местам в которых жили и писали свои произведения художники, поэты и писатели прошлого века, такие как художник И. Айвазовский, К. Богоевский, поэт - художник М. Волошин, писатель А. Грин. В Крыму делаю много зарисовок, эскизов, пишу длительные этюды. Впоследствии этот творческий материал использую в своих работах. В Москве пишу большие работы, очень часто сериями, например серии работ «Сны и видения», «Иллюзии», «Музыка моря», «Лунная соната».
 
     Активной выставочной деятельностью занимаюсь с 2005 г. Провела серию персональных выставок в Москве и за рубежом.
   `,
@@ -32,11 +32,11 @@ const content = {
 
     Love and art are the moving forces of my life. And my lifestyle is the path to artistic decisions to which I haven’t come at once.
 
-    "Every summer I go to Crimea, lead an ascetic life there on a mountain in a tent, do sketches."
+    “Every summer I go to Crimea, lead an ascetic life there on a mountain in a tent, do sketches.“
 
     A little bit of solitude is an attempt to clear your mind from social limits, look at our world with different eyes, see its purity, feel the responsibility and depth of the events in our life. When I return to Moscow, I reevaluate many situations and bring back with me a great stock of inspiration and ideas.
 
-    Almost all my works are dedicated to Crimea, to the places where painters, poets and writers of the last century, such as painter I.Aivazovsky, K.Bogoevsky, poet and painter M.Voloshin, writer A.Grin, lived and created their art. In Crimea I do a lot of sketches and long etudes. Afterwards I use all this creative material in my work. In Moscow I make big paintings, I often paint series of works, for example the series “Dreams and Visions”, “Illusions”, “Music of the Sea, “Moonlight Sonata”.
+    Most of my works are dedicated to Crimea, to the places where painters, poets and writers of the last century, such as painter I.Aivazovsky, K.Bogoevsky, poet and painter M.Voloshin, writer A.Grin, lived and created their art. In Crimea I do a lot of sketches and long etudes. Afterwards I use all this creative material in my work. In Moscow I make big paintings, I often paint series of works, for example the series “Dreams and Visions”, “Illusions”, “Music of the Sea, “Moonlight Sonata”.
 
     I actively participated in exhibitions since 2005. I’ve held a series of personal exhibitions in Moscow and abroad.
   `
