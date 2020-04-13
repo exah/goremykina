ad5c03378ef3e15a29fffba8c916894f54ebab8e
Ivan Grishin <r@exah.me>
2020-04-13 23:52:07 +0100

    ➖ Remove `@babel/plugin-proposal-class-properties`

--- babel.config.js
+++ babel.config.js
@@ -2,29 +2,11 @@ const config = require('config')
 
 module.exports = {
   presets: [
-    [
-      '@babel/preset-env',
-      {
-        useBuiltIns: false,
-        modules: false
-      }
-    ],
+    ['@babel/preset-env', { useBuiltIns: false, modules: false }],
     '@babel/preset-react'
   ],
   plugins: [
-    '@babel/plugin-proposal-class-properties',
-    [
-      '@babel/plugin-transform-runtime',
-      {
-        useESModules: true
-      }
-    ],
-    [
-      'emotion',
-      {
-        sourceMap: config.isDev,
-        autoLabel: config.isDev
-      }
-    ]
+    ['@babel/plugin-transform-runtime', { useESModules: true }],
+    ['emotion', { sourceMap: config.isDev, autoLabel: config.isDev }]
   ]
 }
