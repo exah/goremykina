35ceca4ee6cca44c75578d2b97e30835d22dc985
Ivan Grishin <r@exah.me>
2020-05-10 00:26:29 +0100

    ⬆️ Update `react-universal-data`

--- src/hooks/use-fetch-page.js
+++ src/hooks/use-fetch-page.js
@@ -3,11 +3,14 @@ import { useFetchData } from 'react-universal-data'
 import { fetchPage } from '../api'
 import { useIntl } from './use-intl'
 
+const TTL = 1000 * 60 * 5
+
 export function useFetchPage(slug) {
   const { lang } = useIntl()
 
   return useFetchData(
     useCallback(() => fetchPage({ lang, slug }), [lang, slug]),
-    `page-${slug}`
+    `page-${slug}`,
+    TTL
   )
 }
--- src/hooks/use-fetch-pictures.js
+++ src/hooks/use-fetch-pictures.js
@@ -3,11 +3,14 @@ import { useFetchData } from 'react-universal-data'
 import { fetchPicturs } from '../api'
 import { useIntl } from './use-intl'
 
+const TTL = 1000 * 60 * 5
+
 export function useFetchPictures() {
   const { lang } = useIntl()
 
   return useFetchData(
     useCallback(() => fetchPicturs({ lang }), [lang]),
-    'pictures'
+    'pictures',
+    TTL
   )
 }
