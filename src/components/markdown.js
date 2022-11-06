51188cc8ee6764e725f36951410196d00cf89994
John Grishin <r@exah.me>
2022-11-06 17:54:56 +0000

    💬 Update text

--- src/components/markdown.js
+++ src/components/markdown.js
@@ -1,24 +1,29 @@
 import React, { useMemo } from 'react'
-import { Text } from 'pss-components'
+import { Box, Text } from 'pss-components'
 
 const P = (props) => <Text as='p' mb={2} variant='text' {...props} />
 const H1 = (props) => <P as='h1' variant='title' {...props} />
+const HR = () => <Box as='hr' height='1px' bg='gray' my={2} />
 
-const COMP_PATTERNS = [[H1, /^(#\s)/g]]
+const COMP_PATTERNS = [
+  [H1, /^(#\s)/g],
+  [HR, /^---/g]
+]
 
 const renderMarkdown = (input) =>
   input
     .trim()
     .split('\n\n')
     .map((text, key) => {
-      const match = COMP_PATTERNS.find(([_, pattern]) => pattern.test(text))
+      const trimmed = text.trim()
+      const match = COMP_PATTERNS.find(([_, pattern]) => pattern.test(trimmed))
 
       if (match) {
         const [Comp, pattern] = match
-        return <Comp key={key}>{text.replace(pattern, '')}</Comp>
+        return <Comp key={key}>{trimmed.replace(pattern, '')}</Comp>
       }
 
-      return <P key={key}>{text}</P>
+      return <P key={key}>{trimmed}</P>
     })
 
 export function Markdown({ value }) {
